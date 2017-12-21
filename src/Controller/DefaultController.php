68a915c5955eea21bb3d76827a0ea5216fee1833
Mitchel <mpwassler@gmail.com>
2017-12-20 23:10:32 -0500

    just added, and then removed some code to create a test user

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -11,18 +11,7 @@ use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 class DefaultController extends Controller
 {
     public function index(UserPasswordEncoderInterface $encoder)
-    {
-    	// $em = $this->getDoctrine()->getManager();
-    	// $user = new \App\Entity\User();
-	    // $plainPassword = 'mitchel';
-	    // $encoded = $encoder->encodePassword($user, $plainPassword);
-
-	    // $user->setPassword('mitchel');
-	    // $user->setEmail('mitchel@primaxstudio.com');
-	    
-	    // $em->persist($user);
-	    // // actually executes the queries (i.e. the INSERT query)
-	    // $em->flush();
+    {    	
         return $this->render('index/index.html.twig');
     }
 
